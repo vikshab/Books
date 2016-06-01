54f6250f23c25709ca9c0ad468a3820b04011d0e
Victoria <vikshab2010@hotmail.com>
2016-05-31 18:58:29 -0700

    Added search method, new, create and create_params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,5 +3,35 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
-  
+  def results
+    @query = params[:query]
+    @result = Book.find_by(name: "#{@query}")
+    if @result == nil
+      redirect_to home_path
+      flash[:notice] = "No result for #{@query}"
+    end
+  end
+
+  def new;end
+
+  def create
+    book = Book.create(create_params)
+
+    if !book.save
+      redirect_to :back
+      if book[:name] == "" && book[:author] == "" && book[:description]
+        flash[:notice] = "Field cannot be empty!"
+      elsif book[:author] == ""
+        flash[:notice] = "You forgot author!"
+      elsif book[:description]
+        flash[:notice] = "Please add short description"
+      end
+    else
+      redirect_to books_path
+    end
+  end
+
+  def create_params
+    params.require(:book).permit(:name, :author, :description)
+  end
 end
