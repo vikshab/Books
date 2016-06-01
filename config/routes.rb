69c6c2c2f135e51655c3b8deaba51288d4a26851
Victoria <vikshab2010@hotmail.com>
2016-05-31 19:33:33 -0700

    Small changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
   root 'welcome#index', as: 'home'
   get '/books' => 'books#index', as: 'books'
   get '/books/:book_id/reviews' => 'reviews#index', as: 'reviews'
-  post 'books/:book_id/reviews/' => 'reviews#create', as: 'review_create'
-  get 'books/new' => 'books#new', as: 'new_book_create'
-  post 'books/' => 'books#create', as: 'book_create'
+  post '/books/:book_id/reviews/' => 'reviews#create', as: 'review_create'
+  get '/books/new' => 'books#new', as: 'new_book_create'
+  post '/books/' => 'books#create', as: 'book_create'
 
   post "/search", to: "welcome#search", as: "search"
   get "/books/:query", to: "books#results", as: "books_results"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
