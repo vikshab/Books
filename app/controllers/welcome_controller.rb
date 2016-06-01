81a0ce1f8b5ed10c916c8239c07543b1d85c1c02
Victoria <vikshab2010@hotmail.com>
2016-05-31 19:06:44 -0700

    Added search method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
 
   def index;end
+
+  def search
+    search = params.require(:search).permit(:query)
+    if search[:query] == ""
+      redirect_to :back
+    else
+      redirect_to books_results_path(search[:query])
+    end
+  end
 end
