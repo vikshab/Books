e79bebfecbfd5d44e2c4915b80708bee3bb787b6
Victoria <vikshab2010@hotmail.com>
2016-06-02 17:22:18 -0700

    testing

--- db/migrate/20160530224750_remove_review_from_book.rb
+++ db/migrate/20160530224750_remove_review_from_book.rb
@@ -1,5 +1,5 @@
-class RemoveReviewFromBook < ActiveRecord::Migration
-  def change
-    remove_column :books, :review, :string
-  end
-end
+# class RemoveReviewFromBook < ActiveRecord::Migration
+#   def change
+#     remove_column :books, :review, :string
+#   end
+# end
