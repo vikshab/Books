49a6debc835e9f8d214229f993f4458b71de35ea
Victoria <vikshab2010@hotmail.com>
2016-06-02 17:23:39 -0700

    reverted testing changes

--- db/migrate/20160530223300_create_books.rb
+++ db/migrate/20160530223300_create_books.rb
@@ -1,12 +1,12 @@
-# class CreateBooks < ActiveRecord::Migration
-#   def change
-#     create_table :books do |t|
-#       t.string :name
-#       t.string :description
-#       t.string :author
-#       t.string :review
-#
-#       t.timestamps null: false
-#     end
-#   end
-# end
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.string :description
+      t.string :author
+      t.string :review
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/migrate/20160530230149_create_reviews.rb
+++ db/migrate/20160530230149_create_reviews.rb
@@ -1,11 +1,11 @@
-# class CreateReviews < ActiveRecord::Migration
-#   def change
-#     create_table :reviews do |t|
-#       t.string :text
-#       t.string :author
-#       t.references :book, index: true, foreign_key: true
-#
-#       t.timestamps null: false
-#     end
-#   end
-# end
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.string :text
+      t.string :author
+      t.references :book, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
